529aea8e9426e5ea91d0dc1f76f9e33750860a98
Jordan Pectyo <NaCl4Ever@github.com>
2017-12-22 21:40:02 -0600

    Scaffolding for the website

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PectProjects
 A simple personal blog built for me. 
+I'll be placing work here as I make my way through building my own blog. `
---
+++ javascripts/components/header.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="cruel">
+        Cruel
+        <span>{{ who }}</span>
+    </div>
+</template>
+
+<script>
+module.exports = { // This is important, I got errors when using `export default`
+    data () {
+        return {
+        }
+    }
+}
+</script>
